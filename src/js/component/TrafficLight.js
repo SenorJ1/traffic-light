1c8e9791f17b3b18129333030ea7ac1ae496ecc1
javier s <javier.sibilla.l@gmail.com>
2022-08-01 14:39:28 +0000

    tarea terminada

---
+++ src/js/component/TrafficLight.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+export function TrafficLight()
+{
+
+    return<div>
+            <div id= "Traffic"></div>
+            <div id= "container"></div> 
+            <div className= "red light"></div>
+            <div className= "yellow light"></div>
+            <div className= "green light"></div>
+            </div>
+            
+
+}
+    
+        
+
+
+
+       
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,39 @@
-//import react into the bundle
-import React from "react";
+ 
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function TrafficLight() {
+  const [lights, setLights] = useState([false, false, false]); 
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+  //function setActiveLight(isActive, index) {
+    //if (isActive) {
+     // lights[index] = false;
+     // setLights([...lights]);
+    //} else if (lights.every(active => !active)) {
+     // lights[index] = true;
+    //  setLights([...lights]);
+   // }
+  // }
+
+const [red, setRed] = useState(true); 
+const [yellow, setYellow] = useState(false);
+const [green, setGreen] = useState(false);
+
+  return (
+    <ul className="traffic-light">
+     {/***  {lights.map((isActive, index) => (
+        <li key={index} onClick={() => setActiveLight(isActive, index)}>
+          <span className={isActive ? "on" : "off"} />
+        </li>
+      ))} */}
+      <li className = {red? "redcircleon": "redcircle"} onClick = {() =>{setRed(true);setYellow(false);setGreen(false);}}><span className = "on"> </span></li>;
+      <li className = {yellow? "yellowcircleon" :"yellowcircle"} onClick = {() =>{setYellow(true); setRed(false); setGreen(false);}}><span></span></li>;
+      <li className ={green? "greencircleon": "greencircle"} onClick= {() =>{setGreen(true);setRed(false); setYellow(false);}} ><span></span> </li>;
+    </ul>
+  );
+}
+
+const rootElement = document.getElementById("app");
+ReactDOM.render(<TrafficLight />, rootElement);
